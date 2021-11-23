0893ad46c159ddb6a922e4502ce55f7663698768
remiberger <Ouisticrabedu39@gmail.com>
2021-11-23 11:36:40 +0100

    Ajout ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -31,14 +31,6 @@ self.addEventListener('install', (e) => {
     })());
 });
 
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil((async () => {
-        const cache = await caches.open(cacheName);
-        console.log('[Service Worker] Caching all: app shell and content');
-        await cache.addAll(contentToCache);
-    })());
-});
 
 self.addEventListener('fetch', (e) => {
     e.respondWith((async () => {
@@ -51,4 +43,13 @@ self.addEventListener('fetch', (e) => {
         cache.put(e.request, response.clone());
         return response;
     })());
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key === cacheName) { return; }
+            return caches.delete(key);
+        }))
+    }));
 });
